bc81848c2d2cd4979536a07cb90083d74f4abef3
rs <rainer.schoeffel@seibtundstraub.de>
2022-09-19 15:37:48 +0200

    watchpos

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 var info = 'init';
+var cnt = 0;
+function printResult( s) {
+  document.getElementById('position').textContent = info + ' ' + ++cnt;  
+}
 function geosuccess(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;  
   info = `Lat: ${lat} Lng: ${lng}`;
-  document.getElementById('result').textContent = info;  
+  printResult( info);
 }
 function geofail(){
   info = 'fail';
+  printResult(info);
 }
 function doGeo(){
+  printResult("starting");
  if (navigator.geolocation) {
    navigator.geolocation.getCurrentPosition(geosuccess, geofail); 
  }  
+}
+function startGeo(){
+  printResult('starting warch');
+  navigator.geolocation.watchPosition(geosuccess, geofail, { enableHighAccuracy: true})
 }
 
