c230f1d7951e0d0b1d7daa03a421575861c11f34
rsgmx <69861786+rsgmx@users.noreply.github.com>
2022-09-19 11:40:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var info = 'init';
 function geosuccess(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;  
-  info = 'Lat: ${lat} Lng: ${lng}';
+  info = `Lat: ${lat} Lng: ${lng}`;
   document.getElementById('result').textContent = info;  
 }
 function geofail(){
